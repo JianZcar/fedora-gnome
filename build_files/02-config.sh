f177cc05275edc4dac9cef9901c7029f09c87320
JianZcar <esteban.jianzcar@outlook.com>
2025-08-13 20:38:17 +0800

    usb device wakeup

--- build_files/02-config.sh
+++ build_files/02-config.sh
@@ -15,4 +15,14 @@ echo "Default=true" | tee -a /etc/flatpak/remotes.d/flathub.flatpakrepo > /dev/n
 flatpak remote-add --if-not-exists --system flathub /etc/flatpak/remotes.d/flathub.flatpakrepo
 flatpak remote-modify --system --enable flathub
 
+cat > /usr/lib/bootc/kargs.d/01-mem-sleep.toml <<EOF
+kargs = ["mem_sleep_default=deep"]
+match-architectures = ["x86_64"]
+EOF
+
+cat > /etc/udev/rules.d/01-usb-wakeup.rules <<EOF
+ACTION=="add", SUBSYSTEM=="usb", ATTR{bInterfaceClass}=="03", ATTR{power/wakeup}="enabled"
+ACTION=="change", SUBSYSTEM=="usb", ATTR{bInterfaceClass}=="03", ATTR{power/wakeup}="enabled"
+EOF
+
 echo "::endgroup::"
